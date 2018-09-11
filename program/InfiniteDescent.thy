9b9e73758e3b9e4d321c796d00b8978af90a03de
righ1113 <34955220+righ1113@users.noreply.github.com>
2018-09-11 10:31:22 +0900

    18/09/11 軽微

--- program/InfiniteDescent.thy
+++ program/InfiniteDescent.thy
@@ -3,14 +3,14 @@ imports Main
 begin
 
 theorem infinite_descent [simp]:
-  "(\<forall>n::nat. P(n) \<longrightarrow> (\<exists>m::nat. m<n\<and>P(m))) \<longrightarrow> (\<forall>n::nat. \<not>P(n))"
+  "(\<forall>n. P(n) \<longrightarrow> (\<exists>m. m<n\<and>P(m))) \<longrightarrow> (\<forall>n::nat. \<not>P(n))"
     apply (metis ex_least_nat_le gr_implies_not0)
     done
 
 theorem infinite_descent2 [simp]:
-  "(\<forall>P::nat\<Rightarrow>bool. (\<forall>n::nat. P(n+1) \<longrightarrow> (\<exists>m::nat. m<(n+1)\<and>P(m))) \<longrightarrow> \<not>P(0) \<longrightarrow> (\<forall>n::nat. \<not>P(n)))"
+  "(\<forall>P. (\<forall>n. P(n+1) \<longrightarrow> (\<exists>m::nat. m<n+1 \<and> P(m))) \<longrightarrow> \<not>P(0) \<longrightarrow> (\<forall>n::nat. \<not>P(n)))"
     (* sledgehammer *)
-    apply (smt discrete ex_least_nat_le ex_least_nat_less nat_less_le)
+    apply (metis Suc_eq_plus1 Suc_leI ex_least_nat_less not_le)
     done
 
 
